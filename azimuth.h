2aa8489d8a1198d62050f9d5d52ce1d5b9f8a2bf
Jacob M. Smith <jmsmith6101@att.net>
2023-02-25 11:58:31 -0500

    clarifying comments

--- azimuth.h
+++ azimuth.h
@@ -1,6 +1,6 @@
 #define two_pi (2 * M_PI)
 
 double azimuth_spherical(double theta, double reference_angle);             // find the azimuth for a set of spherical coordintates and arbitrary reference angle
-double azimuth_polar(double theta, double reference_angle);
-double azimuth_cartesian(double x, double y, double reference_angle);
-void azimuth_reference_angle_bounds(double* reference_angle);
+double azimuth_polar(double theta, double reference_angle);                 // find the azimuth for a set of polar coordinates and arbitrary reference angle
+double azimuth_cartesian(double x, double y, double reference_angle);       // find the azimuth for a set of cartesian coordinates and arbitrary reference angle
+void azimuth_reference_angle_bounds(double* reference_angle);               // bound correction for angles if necessary (probably useless)
