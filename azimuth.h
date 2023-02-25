c2f7df6cf01164e9f04b64be85b1491c283c96a3
Jacob M. Smith <jmsmith6101@att.net>
2023-02-25 12:05:15 -0500

    updated function definitions

--- azimuth.h
+++ azimuth.h
@@ -6,9 +6,9 @@ double azimuth_polar(double theta, double reference_angle);                 // f
 double azimuth_cartesian(double x, double y, double reference_angle);       // find the azimuth for a set of cartesian coordinates and arbitrary reference angle
 
 // calculations from due north
-double azimuth_spherical(double theta);                                     // find the azimuth for a set of spherical coordintates from due north
-double azimuth_polar(double theta);                                         // find the azimuth for a set of polar coordinates from due north
-double azimuth_cartesian(double x, double y);                               // find the azimuth for a set of cartesian coordinates from due north
+double azimuth_spherical_from_due_north(double theta);                      // find the azimuth from due north for a set of spherical coordintates 
+double azimuth_polar_from_due_north(double theta);                          // find the azimuth from due north for a set of polar coordinates
+double azimuth_cartesian_from_due_north(double x, double y);                // find the azimuth from due north for a set of cartesian coordinates 
 
 // helpers
 void azimuth_reference_angle_bounds(double* reference_angle);               // bound correction for angles if necessary (probably useless)
