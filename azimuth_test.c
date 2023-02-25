2f5bc7a4bafeb08b5a993ce4539a205e4551c399
Jacob M. Smith <jmsmith6101@att.net>
2023-02-25 12:16:29 -0500

    created testing skeleton

---
+++ azimuth_test.c
@@ -0,0 +1,21 @@
+int main()
+{
+    int pass = 1;
+
+    return pass;
+}
+
+int spherical_with_arbitrary_ref_test(double theta, double reference_angle)
+{
+    return 0;
+}
+
+int polar_with_arbitrary_ref_test(double theta, double reference_angle)
+{
+    return 0;
+}
+
+int cartesian_with_arbitrary_ref_test(double x, double y, double reference_angle)
+{
+    return 0;
+}
