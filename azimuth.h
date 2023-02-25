b53392180a03c6b3c7d4c56fb7f47a7bbf8a59a6
Jacob M. Smith <jmsmith6101@att.net>
2023-02-25 11:55:49 -0500

    modified parameters for sperical fucntion

--- azimuth.h
+++ azimuth.h
@@ -1,6 +1,6 @@
 #define two_pi (2 * M_PI)
 
-double azimuth_spherical(double r, double theta, double phi);
+double azimuth_spherical(double theta, double reference_angle);             // find the azimuth for a set of spherical coordintates and arbitrary reference angle
 double azimuth_polar(double theta, double reference_angle);
 double azimuth_cartesian(double x, double y, double reference_angle);
 void azimuth_reference_angle_bounds(double* reference_angle);
